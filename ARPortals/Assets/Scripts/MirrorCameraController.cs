468a698d9feecc0fbd05698c50a93bcf42c24632
LB31 <leonidb93@gmail.com>
2018-04-18 15:38:18 +0200

    Add door with room reflection

---
+++ ARPortals/Assets/Scripts/MirrorCameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorCameraController : MonoBehaviour {
+    public GameObject ARCamera;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // transform.eulerangles = arcamera.transform.eulerangles;
+        transform.rotation = Quaternion.LookRotation(ARCamera.transform.forward);
+    }
+}
---
+++ ARPortals/Assets/Scripts/TeleportPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayer : MonoBehaviour {
+    public GameObject digitalWorld;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   
+    void OnTriggerEnter(Collider other)
+     {
+        digitalWorld.layer = 0;
+        Debug.Log("Triggered");
+     }
+    
+
+
+}
