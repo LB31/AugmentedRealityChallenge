9258a2adfddcdd5cb6b7540cc413ee92d0f79182
LB31 <leonidb93@gmail.com>
2018-04-18 16:51:58 +0200

    Add information text. Currently too small

---
+++ ARPortals/Assets/Scripts/NotificationsHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificationsHandler : MonoBehaviour {
+    public GameObject text;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.GetComponent<BoxCollider>().enabled == true)
+        {
+            text.SetActive(false);
+        }
+
+    }
+
+}
